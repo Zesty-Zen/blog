05b9d17d5e65c99cd670a8eb17cb412ce5b458b8
Zesty-Zen <ibrahimmamoon7@gmail.com>
2024-03-18 21:07:49 +0500

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   
 
-
+  
   root "articles#index"
   resources :articles do
     resources :comments
   end
-  # get "/articles", to: "articles#index"  
   # get "/articles/:id", to: "articles#show"
-
+  
+  # get "/articles", to: "articles#index"  
   # get 'articles/index'
   
   
