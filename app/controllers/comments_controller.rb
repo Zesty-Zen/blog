4bad184b78bbaa79e606e72e283c7dbe397d9b79
Zesty-Zen <ibrahimmamoon7@gmail.com>
2024-03-21 20:40:46 +0500

    comment controller fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,17 @@
 class CommentsController < ApplicationController
-    
-    # http_basic_authenticate_with name: "Ibrahim", password: "0000", only: :destroy
-    
+        
     expose :article
     expose :comments , -> { article.comments }
     expose :comment
 
     def create
-        # comment = comments.create(comment_params)
+        comment.article_id = article.id
         if comment.save
             redirect_to article_path(article)
         end
     end
     
     def destroy      
-        # comment = comments.find(params[:id])
         if comment.destroy
             redirect_to article_path(article), status: :see_other
         end       
